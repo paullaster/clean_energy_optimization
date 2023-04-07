67b23c9210eb870fd0d4dd96c037614247299b5e
paullaster <paullasterokoth98@gmail.com>
2023-04-07 16:19:47 +0300

    scafold prototype files

---
+++ src/views/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div>
+        <h1>Error</h1>
+        <p>We are finding trouble loading this page</p>
+    </div>
+  )
+}
+
+export default Error
