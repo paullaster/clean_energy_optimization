a1eb96b4b3bbf10cb25595d505e011e50350609d
paullaster <paullasterokoth98@gmail.com>
2023-04-07 16:13:20 +0300

    scafold prototype files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { HourlyEnergyCurve } from './components'
+import { HourlyEnergyCurve } from './components',
+import HourlyEnergyData from './db/daily_energy_stat.json';
 
 const App = () => {
   return (
