fc5b281bb31c9aeef994cc776b3156b82412c13c
paullaster <paullasterokoth98@gmail.com>
2023-04-07 17:09:19 +0300

    scafold prototype files

--- src/components/DailyLineCurveChart.jsx
+++ src/components/DailyLineCurveChart.jsx
@@ -13,40 +13,9 @@ const DailyLineCurveChart = ({ width, height, options }) => {
             chart.resize({width, height});
             chart.setOptions(options);
         }
-    }, [])
+    }, [chart, options, width, height])
 
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "top"
-      },
-      title: {
-        display: "true",
-        text: "Daily Clean Energy Production in Barrels"
-      }
-    },
-    options: {
-        scale: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-  };
-
-  const data = {
-    labels,
-    dataset: [
-      {
-        lable: "Hourly Clean Energy Levels in Barrels",
-        data: plotData,
-        borderWidth: 1,
-        borderColor: "rgba(255, 25, 5)",
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
-      }
-    ]
-  };
+  
   return <div ref={chartHolder}></div>;
 };
 
